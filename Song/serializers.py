ad5d42d84272c93c74c7ae361353f606f3b4a45a
Usman Farooq <usmanfarooqwork@gmail.com>
2023-07-30 00:55:03 +0500

    Added the ability to request a Song to be created and also made An input Serilizer for SONG Model to aid in doing so

--- Song/serializers.py
+++ Song/serializers.py
@@ -1,5 +1,9 @@
+from typing import Dict, List
+
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 
+from Artist.models import Artists
 from Artist.serializers import ArtistOutPutSerializer
 from Song.models import Songs
 
@@ -27,3 +31,70 @@ class SongOutPutSerializer(serializers.ModelSerializer):
         fields = ["name", "rating", "sung_by_artists"]  # the field "sung_by_artists"
         # is going to be an Array of Artist Objs, instead of a SINGULAR Artist Obj because of the
         # "many-to-many" cardinality of the "Song <--> Artist" relation
+
+
+class SongInPutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Songs
+        fields = ['name_of_artists', 'name']  # for a Song to be created,
+        # ALL these fields must be provided (unless told otherwise inside the models.py file where these Fields are actually defined),
+        # this now obviously means name_of_artists is something that will NEVER be a "MUST to provide" because
+        # it is not mentioned inside the models.py file, BUT because we set required=True
+        # in name_of_artists = serializers.ListField(required=True).. now name_of_artists IS
+        # a "MUST to provide" key in request JSON
+
+    name_of_artists = serializers.ListField(required=True)  # required=True validates/forces name_of_artists be sent all the time by user JSON request
+
+    # def validate_name_of_artists is called by is_valid > run_validations > to_internal_value
+    # note the validate_<field_name> nomenclature
+    def validate_name_of_artists(self, value: List) -> List[Artists]:
+        artists_found = []
+        for artist in value:
+            try:
+                an_artist = Artists.objects.get(first_name=artist)
+            except ObjectDoesNotExist as DoesNotExist:
+                raise serializers.ValidationError(f"An artist with the name={artist} does not exist")
+            artists_found.append(an_artist)
+        return value  # e are returning back List of str instead of List of Artist Objs
+        # because we want self.validated_data["name_of_artists"] to be List of str instead of List of Artists
+        # because all custom-attrs of a InputSerializer should be of Python primitive Types
+
+    # this method below is called iff both the conditions are fulfilled:
+    # Condition-1) we instantiated the Serializer like so=ArtistInPutSerializer(data=data, many=<BOOL>)
+    # Condition-2) we called the .save method on the aforementioned Serializer instance
+    # and all this method will do is that it will create a new instance inside the DB.
+    def create(self, validated_data: Dict):
+        # remember the "validated_data" attr of the Serializer object contains whatever we list inside the "fields" attr of the Meta sub-Class
+        # that we define inside the Serializer **SET-INTERSECT** the stuff we pass to the data param when
+        # instantiating the serializers, and once that Data has
+        # successfully passed through the appropriate validators
+        # that have been written for those attributes of the Serializer
+
+        # whereas the "data" is the stuff we pass to the data param when
+        # instantiating the serializers, and once that Data has
+        # successfully passed through the appropriate validators
+        # that have been written for those attributes of the Serializer **SET-UNION** whatever we list inside the "fields" attr of the Meta sub-Class
+        # that we define inside the Serializer
+        song_to_create = Songs(name=validated_data['name'], rating=3.0)
+        song_to_create.save()
+        artists_names = validated_data["name_of_artists"]  # depends on what def validate_name_of_artists returns
+        for artist_name in artists_names:
+            an_artist = Artists.objects.get(first_name=artist_name)
+            song_to_create.sung_by_artists.add(an_artist)
+        return song_to_create
+
+    # th "update" method below is called iff both the conditions are fulfilled:
+    # Condition-1) we instantiated the Serializer like so=ArtistInPutSerializer(Artist_instance, data=data, many=False)
+    # Condition-2) we called the .save method on the aforementioned Serializer instance
+    # and all this method will do is that it will update an existing instance inside the DB.
+    def update(self, instance: Artists, validated_data: Dict):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.stage_name = validated_data.get('stage_name', instance.stage_name)
+        instance.age = validated_data.get('age', instance.age)
+        instance.save()
+        return instance
+
+    # # def validate is called at the very end of validation process
+    # def validate(self, data):
+    #     raise serializers.ValidationError("<Blah blah Blah>")
--- Song/views.py
+++ Song/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from Song.models import Songs
-from Song.serializers import SongOutPutSerializer
+from Song.serializers import SongOutPutSerializer, SongInPutSerializer
 
 
 # Create your views here.
@@ -15,3 +15,20 @@ class ViewSong(APIView):
         song_qs = Songs.objects.all()
         song_result_dict = SongOutPutSerializer(song_qs, many=True)
         return Response(song_result_dict.data)
+
+    def post(self, request):
+        """Create A Song"""
+        data_to_make_song_from = request.data
+        data_serializer = SongInPutSerializer(data=data_to_make_song_from)
+
+        # DRF enforces data validation in the deserialization process,
+        # which is why you need to call is_valid() before accessing the validated data.
+        # If the data is invalid, errors are then appended to
+        # the serializer's error property and a ValidationError is thrown.
+        if data_serializer.is_valid(raise_exception=True):
+            # just a side note --> You cannot call `.save()` after
+            # accessing `serializer.data`.If you need to
+            # access data before committing to the database then inspect 'serializer.validated_data' instead.
+            data_serializer.save()  # create() method of the serializer will be called now
+            validated_data_dict = data_serializer.validated_data
+            return Response(data=validated_data_dict, status=200, exception=True)
